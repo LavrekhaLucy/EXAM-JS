a7b93f393573307a9e502c7580cc52bccbb44b02
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-08-21 21:04:36 +0300

    task 2/ users - 1,2,3 done

---
+++ task_2/js/main.js
@@ -0,0 +1,37 @@
+    const usersContainer = document.getElementsByClassName("users-container")[0];
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(users => {
+
+            for (const user of users) {
+
+                let cartDiv = document.createElement('div');
+                cartDiv.className = ('user-card');
+
+                let p = document.createElement('p');
+                p.innerText = user.id;
+
+                let h3 = document.createElement('h3');
+                h3.innerText = user.name;
+
+
+
+                let link = document.createElement("a");
+                link.href = `user-details.html?id=${user.id}`;
+                link.innerText = "Деталі користувача";
+                link.className = "user-link";
+
+
+                cartDiv.append(p,h3,link);
+                usersContainer.appendChild( cartDiv);
+
+                console.log(user);
+            }
+
+        })
+
+
+
+
+
---
+++ task_2/js/user-details.js
@@ -0,0 +1,23 @@
+// const urlParams = new URLSearchParams(window.location.search);
+// const userId = urlParams.get("id"); // <-- має бути число, не null
+//
+// if (!userId) {
+//     console.error("User ID not found in URL!");
+// } else {
+//     fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+//         .then(res => res.json())
+//         .then(user => {
+//             console.log(user);
+//
+//             let container = document.getElementById("user-details");
+//             container.innerHTML = `
+//                 <h2>${user.name} (ID: ${user.id})</h2>
+//                 <p><b>Username:</b> ${user.username}</p>
+//                 <p><b>Email:</b> ${user.email}</p>
+//                 <p><b>Phone:</b> ${user.phone}</p>
+//                 <p><b>Website:</b> ${user.website}</p>
+//                 <p><b>Company:</b> ${user.company.name}</p>
+//                 <p><b>City:</b> ${user.address.city}</p>
+//             `;
+//         });
+// }
