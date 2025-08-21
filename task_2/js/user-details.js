e82f2d860efe5749ade44c8fae0f3f53c16b723d
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-08-21 23:23:19 +0300

    task 2/ users-details

--- task_2/js/user-details.js
+++ task_2/js/user-details.js
@@ -1,18 +1,9 @@
-    //
-    // fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-    //     .then(res => res.json())
-    //     .then(user => {
-    //         console.log(user);
-    //
-    //         let container = document.getElementById('user-details');
-    //         container.innerHTML = `
-    //             <h2>${user.name} (ID: ${user.id})</h2>
-    //             <p><b>Username:</b> ${user.username}</p>
-    //             <p><b>Email:</b> ${user.email}</p>
-    //             <p><b>Phone:</b> ${user.phone}</p>
-    //             <p><b>Website:</b> ${user.website}</p>
-    //             <p><b>Company:</b> ${user.company.name}</p>
-    //             <p><b>City:</b> ${user.address.city}</p>
-    //         `;
-    //     });
-    //
+    let url = new URL(`https://jsonplaceholder.typicode.com/users`);
+       console.log(url);
+
+    fetch(url)
+        .then(res => res.json())
+        .then(user => {
+            console.log(user);
+        });
+
